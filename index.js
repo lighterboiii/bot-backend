71d739882c0a79539fbdb88c536abdc9251858af
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-02-28 12:16:13 +0300

    gt

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const PORT = 8000;
 const token = '6861958515:AAFtLJnKgI0qXOoyc2UdlQKbuWxp--kHCFA';
 const bot = new TelegramBot(token, { polling: true });
-const URL = 'https://lighterboiii.github.io/vpn-bot/';
+// const URL = 'https://lighterboiii.github.io/vpn-bot/';
+const URL = 'https://main--vpnstool.netlify.app';
 const app = express();
 
 app.use(express.json());
@@ -13,18 +14,18 @@ app.use(cors());
 
 const removeTyPrefix = (inputString) => {
   if (inputString.toLowerCase().startsWith("ты ")) {
-      return inputString.slice(3).trim();
+    return inputString.slice(3).trim();
   } else {
-      return inputString;
+    return inputString;
   }
 };
 
 const returnAnswer = (string) => {
-  if (string.toLowerCase().startsWith("ты ")) { 
+  if (string && string.toLowerCase().startsWith("ты ")) {
     const changedString = removeTyPrefix(string);
-    return 'А может быть ты '+ changedString.toLowerCase() + ' или подумаешь ещё?';
+    return 'А может быть ты ' + changedString.toLowerCase() + ' или подумаешь ещё?';
   } else {
-    return 'А может быть ты '+ string.toLowerCase() + ' ёпта?';
+    return 'А может быть ты ' + string?.toLowerCase() + ' ёпта?';
   }
 }
 
@@ -35,58 +36,55 @@ bot.on('message', async (msg) => {
   if (text === '/start') {
     await bot.sendMessage(chatId, 'Оформить подписку на наш VPN-сервис или попробовать его бесплатно можно по кнопке ниже', {
       reply_markup: {
-        inline_keyboard: [
-          [{ text: 'Кнопка ниже', web_app: { url: URL } }]
+        keyboard: [
+          [{ text: 'Запустить приложение', web_app: { url: `${URL}/signup` }}]
         ]
       }
     });
   } else {
-    bot.sendMessage(chatId, returnAnswer(text), {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: 'Лучше вот сюда вот нажми', web_app: { url: URL } }]
-        ]
-      }
-    });
+    if (!msg?.web_app_data?.data) {
+      bot.sendMessage(chatId, returnAnswer(text), {
+        reply_markup: {
+          keyboard: [
+            [{ text: 'Нажми на кнопочку', web_app: { url: URL } }]
+          ]
+        }
+      });
+    }
   }
-  await bot.sendMessage(chatId, {
-    reply_markup: {
-      keyboard: [
-        [{ text: 'Запустить приложение', web_app: { url: URL } }]
-      ]
-    }
-  });
 
-  if(msg?.web_app_data?.data) {
+  if (msg?.web_app_data?.data) {
     try {
-        const data = JSON.parse(msg?.web_app_data?.data)
-        console.log(data)
-        await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+      const data = JSON.parse(msg?.web_app_data?.data)
+      data.name ? 
+      await bot.sendMessage(chatId, `Привет, ${data?.name}`) :
+      await bot.sendMessage(chatId, `Привет`)
 
-        setTimeout(async () => {
-            await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
-        }, 3000)
+      setTimeout(async () => {
+        await bot.sendMessage(chatId, `Твоя почта: ${data?.email}`);
+        await bot.sendMessage(chatId, `Твой номер: ${data?.phone}`);
+      }, 300)
     } catch (e) {
-        console.log(e);
+      console.log(e);
     }
-}
+  }
 });
 
 app.post('/web-data', async (req, res) => {
   const { queryId, selectedPlan } = req.body;
   try {
-      await bot.answerWebAppQuery(queryId, {
-          type: 'article',
-          id: queryId,
-          title: 'Успешно',
-          input_message_content: {
-              message_text: ` Поздравляем с покупкой. Ваша подписка - ${selectedPlan.name}}`
-          }
-      })
-      return res.status(200).json({});
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: 'Успешно',
+      input_message_content: {
+        message_text: `Поздравляем с покупкой. Ваша подписка ${selectedPlan.name}`
+      }
+    })
+    return res.status(200).json({});
   } catch (e) {
-      console.log(e);
-      return res.status(500).json({});
+    console.log(e);
+    return res.status(500).json({});
   }
 });
 
